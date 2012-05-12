103cc1e84e974c089145c742ab8a157cabc408a5
bigteddy <janni-futz@ewetel.net>
2012-05-12 18:52:48 +0200

    iasud

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,13 @@
 module ApplicationHelper
 	def markdown(text)
 		markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, :autolink => true)
-  		markdown.render (text)
+  		html_safe markdown.render (text)
+	end
+
+	def html_safe(text)
+       return text if text.nil?
+       return text.html_safe if defined?(ActiveSupport::SafeBuffer)
+       return text.html_safe! if text.respond_to?(:html_safe!)
+       text
 	end
 end
